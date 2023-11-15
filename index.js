65bf7b9c16d21d9f575b20db824069b54f419095
oliullah05 <mdolihasan@gmail.com>
2023-11-15 22:37:21 +0600

    movies data get done

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 2000;
+
+
+//middle wars
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Advanced-Filter-Pagination-And-Search-System:uRCA1zNpMFRcyY14@cluster0.iono61s.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+  const moviesCollection = client.db("Advanced-Filter-Pagination-And-Search").collection("moviesData");
+
+    app.get("/",(req,res)=>{
+        res.send(`server is running`)
+    })
+
+    app.get("/movies",async(req,res)=>{
+       const result = await moviesCollection.find().toArray();
+       res.send(result)
+    })
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+})
